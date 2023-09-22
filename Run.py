5c75f379683772be63b0e65fdbb6f898409e61d4
goblin588 <brendanwallis01@gmail.com>
2023-09-22 14:43:25 +1000

    Updated for PsyPySimAnneal Testing

--- Run.py
+++ Run.py
@@ -5,7 +5,7 @@ import Controller
 
 # # Initialize experiment and algorithm
 experiment = Experiment.Experiment()
-simulated_annealing_algorithm = OptimizationAlgorithm.SimulatedAnnealing(experiment)
+simulated_annealing_algorithm = OptimizationAlgorithm.SciPiSimAnneal(experiment)
 
 # Create controller and run optimization
 controller = Controller.Controller(experiment, simulated_annealing_algorithm)
