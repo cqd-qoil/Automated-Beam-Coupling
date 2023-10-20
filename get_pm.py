c43a9ccfcbf766f58b407ab516154ec803a2e47d
Brendan Wallis <brendanwallis01@gmail.com>
2023-10-20 15:47:53 +1000

    Updates get_pm

--- get_pm.py
+++ get_pm.py
@@ -1,30 +1,38 @@
 import pyvisa as visa
 
-def get_power_meter_addresses():
+def get_power_meter_address(connection_method, COM_port=None):
+    """
+    Takes connection_method argument ('USB' or 'COM') and optionally COM_port='COM#' if using COM
+    """
     rm = visa.ResourceManager()
-    pm_addresses = []
+    pm_addr = None
+    
+    # Dictionary for different connection methods and their address formats
+    address_formats = {
+        'USB': 'USB0::0x0000::0x0000::0::INSTR',
+        'COM': f'ASRL{COM_port}::INSTR'
+    }
+    
+    # Get the corresponding address format
+    address = address_formats.get(connection_method)
+    
+    if address is None:
+        print(f"Invalid connection method: {connection_method}")
+        return None
+    
+    if 'COM' in connection_method and COM_port is not None:
+        address = address.format(COM_port=COM_port)
 
-    # Search through all VISA resources
-    for item in rm.list_resources():
-        try:
-            inst = rm.open_resource(item)
-            idn = inst.query('*IDN?').strip()
-            print(f"VISA Resource: {item}, IDN: {idn}")  # Print information about detected devices
-            if 'PM100D' in idn or 'PM160' in idn:  # Replace with the specific models of your power meters
-                pm_addresses.append(item)
-        except Exception as e:
-            print(f"Error querying VISA resource {item}: {e}")
-
-    if not pm_addresses:
-        print("No power meters found.")
-        return []
-
-    # If you know the COM port of one of them, you can directly add it
-    known_com_port = "COM3"  # replace with your known COM port
-    pm_addresses.append(f"ASRL{known_com_port}::INSTR")
-
-    print(f"Power meter VISA addresses: {pm_addresses}")
-    return pm_addresses
+    try:
+        inst = rm.open_resource(address)
+        idn = inst.query('*IDN?').strip()
+        print(f"VISA Resource: {address}, IDN: {idn}")  # Print information about detected device
+        if 'PM100D' in idn:  # Replace 'PM100D' with the specific model of your power meter
+            return address
+    except Exception as e:
+        print(f"Error querying VISA resource {address}: {e}")
+        print("Power meter not found.")
+        return None
 
 # Example usage
-pm_addresses = get_power_meter_addresses()
+pm_addresses = get_power_meter_addresses('USB', COM_port=None)
