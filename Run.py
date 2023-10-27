c2a04a2b9dee0283d1f181484a4177cd4a92cd61
Brendan Wallis <brendanwallis01@gmail.com>
2023-10-27 11:41:13 +1000

    Added database handling and init

--- Run.py
+++ Run.py
@@ -2,10 +2,12 @@
 import Experiment
 import OptimizationAlgorithm
 import Controller
+from Database import Database
 
 # # Initialize experiment and algorithm
 experiment = Experiment.Experiment()
-basin_hopping_result = OptimizationAlgorithm.BasinHopping(experiment)
+database = Database()
+basin_hopping_result = OptimizationAlgorithm.BasinHopping(experiment, database)
 
 # Create controller and run optimization
 controller = Controller.Controller(experiment, basin_hopping_result)
