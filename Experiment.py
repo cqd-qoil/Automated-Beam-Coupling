648d056f32d2bb96723d372668e3327947fcdd87
goblin588 <brendanwallis01@gmail.com>
2023-10-27 17:13:29 +1000

    Fixed detector reading

--- Experiment.py
+++ Experiment.py
@@ -16,19 +16,23 @@ class Experiment:
         self.motors.open_motor_connection()
 
         #Variable Initialisation
-        self.solution = self.get_motor_coordinates()
         self.filtering_coefficient = 76e-6
 
-    def move_to_array(self):
-        self.motors.move_to_array(self.solution)
+        # print("Starting Motor Coordinates: ", self.motors.get_motor_coordinates)
+
+    def move_to_array(self, solution):
+        self.motors.move_to_array(solution)
         
     def get_motor_coordinates(self):
         return self.motors.get_motor_coordinates()
 
     def evaluate_solution(self, solution):
-        return self.detector.read()
+        return self.detector1.read()
     
     def benchmark(self):
         #Theoretical maximum pairing based off heralded detector split ration + filtering aspect
         p_opt = (1.185/4.640) * self.detector2.read() #* self.filtering_coefficient
         return self.detector1.read()/p_opt
+
+    def close_motor_connection(self):
+        self.motors.close_motor_connection()
