d00ac67232d57a31a039475d6b176e0fe93f109e
goblin588 <brendanwallis01@gmail.com>
2023-11-03 17:11:58 +1000

    added methodLoop to trial opt methods

--- Controller.py
+++ Controller.py
@@ -1,22 +1,55 @@
 import Database
+import time
 
 # Main Controller
 class Controller:
     def __init__(self, experiment, algorithm):
         self.experiment = experiment
         self.algorithm = algorithm
+        self.database = Database.Database()
     
     def run(self, experiment):
-        optimal_solution = self.algorithm.optimize()
-        # Use the optimal solution, e.g., align mirrors accordingly
-        print("Optimal solution found at:", optimal_solution)
-        experiment.close_motor_connection()
-        print("Optimal solution should be: ", self.experiment.benchmark())
+        self.method_loop(experiment)
+        # try:
+        #     solution = self.algorithm.optimize(method)
+        #     experiment.move_to_array(solution.x)
+        # finally:
+        #     experiment.close_motor_connection()
+        # # Use the optimal solution, e.g., align mirrors accordingly
+        # print("Solution found at:", solution.fun * -1000, "Optimal solution should be: ", self.experiment.benchmark())
+
+    def method_loop(self, experiment):
+        methods_full = ['Powell', 'Nelder-Mead', 'CG', 'BFGS', 'Newton-CG', 'L-BFGS-B', 'TNC', 
+                   'COBLYA', 'SLSQP', 'trust-constr', 'dogleg', 'trust-ncg', 'trust-exact', 'trust-krylov']
+        
+        methods = ['Powell', 'Nelder-Mead', 'trust-krylov']
+
+        final_couplings = []
+    
+        try:
+            for i in range(len(methods)): 
+                self.reset_pairing
+                method = methods[i]
+                solution = self.algorithm.optimize(method)
+                experiment.move_to_array(solution.x)
+                # Use the optimal solution, e.g., align mirrors accordingly
+                print(methods[i], " solution found at:", solution.fun * -1000, "Optimal solution should be: ", self.experiment.benchmark())
+                final_couplings.append(solution.fun)
+        finally:
+            print("Experiment Concluding...")
+            print(methods)
+            print(final_couplings)
+            experiment.close_motor_connection()
+    
+    def reset_pairing(self, experiment):
+        paired = [7632.0, 14132.0, -5695.0, -6508.0]
+        # motors.reset_motor_axis()
+        experiment.move_to_array(paired)
+        time.sleep(2)
 
     def model(self):
         self.database.print()
         self.database.modelCountVsTime()
         self.database.modelCountVsAxis()
-        #self.database.modelTimeVsAxis()
         
 
