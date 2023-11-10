15329320d02f55fb6719e35900c35406c7780caf
goblin588 <brendanwallis01@gmail.com>
2023-11-10 17:14:22 +1000

    Refactored for method cycling

--- Run.py
+++ Run.py
@@ -5,13 +5,51 @@ import Controller
 from Database import Database
 
 # # Initialize experiment and algorithm
-experiment = Experiment()
-database = Database()
-basin_hopping_result = OptimizationAlgorithm.BasinHopping(experiment, database)
+# experiment = Experiment()
+# database = Database()
+# iterations = 10
+# method = 'Neal-Melder'
+# basin_hopping_result = OptimizationAlgorithm.BasinHopping(experiment, database, iterations, method)
 
 # Create controller and run optimization
-try:
-    controller = Controller.Controller(experiment, basin_hopping_result, database)
-    controller.run(experiment)
-finally:
-    controller.model()
+# try:
+#     controller = Controller.Controller(experiment, basin_hopping_result, database)
+#     controller.run(experiment)
+# finally:
+#     controller.model()
+
+def all_methods():
+    paired = [7632.0, 14132.0, -5695.0, -6508.0]
+    # motors.reset_motor_axis()
+    methods = ['Nelder-Mead']
+    #some of these need extra
+    # methods = ['Powell', 'Nelder-Mead', 'CG', 'BFGS', 'Newton-CG', 'L-BFGS-B', 'TNC', 
+                #    'COBLYA', 'SLSQP', 'trust-constr', 'dogleg']
+    for i in range(len(methods)):    
+        # # Initialize experiment and algorithm
+        experiment = Experiment()
+        #Reset solution to pairing coordinates
+        experiment.move_to_array(paired)
+        database = Database()
+        iterations = 10
+        niter_success = 6
+        temp = 1
+        basin_hopping_result = OptimizationAlgorithm.BasinHopping(experiment=experiment, 
+                                                                  database=database, 
+                                                                  iters=iterations, 
+                                                                  method=methods[i], 
+                                                                  niter_success=niter_success, 
+                                                                  temp=temp)
+        database.setMethod(methods[i])
+        # Create controller and run optimization
+        try:
+            controller = Controller.Controller(experiment, basin_hopping_result, database)
+            controller.run(experiment)
+        finally:
+            controller.model()
+
+all_methods()
+ 
+
+
+   
