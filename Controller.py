57eaad766dfacde6618f782bdc76d666f8c2d846
goblin588 <brendanwallis01@gmail.com>
2023-11-10 17:11:44 +1000

    Refactored cycling methods functionality

--- Controller.py
+++ Controller.py
@@ -9,43 +9,14 @@ class Controller:
         self.database = database
     
     def run(self, experiment):
-        # self.method_loop(experiment)
-        method = 'Nelder-Mead'
         try:
-            solution = self.algorithm.optimize(method)
+            solution = self.algorithm.optimize()
             experiment.move_to_array(solution.x)
         finally:
             experiment.close_motor_connection()
         # Use the optimal solution, e.g., align mirrors accordingly
         print("Solution found at:", solution.fun * -1000, "Optimal solution should be: ", self.experiment.benchmark())
-
-    def method_loop(self, experiment):
-        methods_full = ['Powell', 'Nelder-Mead', 'CG', 'BFGS', 'Newton-CG', 'L-BFGS-B', 'TNC', 
-                   'COBLYA', 'SLSQP', 'trust-constr', 'dogleg']
-        
-        methods = ['Powell', 'Nelder-Mead']
-
-        final_couplings = []
-        """
-        Need to add different database for every method to get seperate graphs
-        OR 
-        model inside for loop
-        """
-        try:
-            for i in range(len(methods)): 
-                self.reset_pairing
-                method = methods[i]
-                solution = self.algorithm.optimize(method)
-                experiment.move_to_array(solution.x)
-                # Use the optimal solution, e.g., align mirrors accordingly
-                print(methods[i], " solution found at:", solution.fun * -1000, "Optimal solution should be: ", self.experiment.benchmark())
-                final_couplings.append(solution.fun)
-        finally:
-            print("Experiment Concluding...")
-            print(methods)
-            print(final_couplings)
-            experiment.close_motor_connection()
-    
+ 
     def reset_pairing(self, experiment):
         paired = [7632.0, 14132.0, -5695.0, -6508.0]
         # motors.reset_motor_axis()
@@ -53,7 +24,7 @@ class Controller:
         time.sleep(2)
 
     def model(self):
-        self.database.print()
+        # self.database.print()
         self.database.modelCountVsTime()
         self.database.modelCountVsAxis()
         
