6041df8d0807e098628b3d857b26548984be82ef
luyves <knz@tutanota.com>
2024-05-28 11:14:21 +1000

    Changed counting logic and antilatch behavior

--- src/Detector.py
+++ src/Detector.py
@@ -129,11 +129,14 @@ class Logic16(Detector):
         self._total_channels = self.MyTagger.GetNoInputs()
         self._integration_window = 0.5 # same as self.timeInterval
         self._coincidence_window = 1e-9
+        # For antilatch
         self.singles = None
+        self._antilatch_timeslice = 0.100 # 100 miliseconds
+        self.antilatch_func = lambda: print('No antilatch function set.')
         self.coincidences = None
-
         self.TimerCounter1 = Int32
         self.clear_buffer()
+        self.verbose = False
 
     def __enter__(self):
         return self
@@ -190,26 +193,81 @@ class Logic16(Detector):
         self.MyLogic.ReadLogic()
         TimeCounter1 = self.MyLogic.GetTimeCounter()
 
-    def read_counts(self, channel_pos, channel_neg=0, normalize=False):
+    def calc_single_count(self, pos, neg):
+        return self.MyLogic.CalcCount(binary_code(pos), binary_code(neg))
+
+    def _read_counts(self, channel_pos, channel_neg=0, normalize=False):
         """
-        Reads Counts for time interval
+        Obsolete. Kept for Simon.
         """
         self.MyLogic.ReadLogic()
         TimeCounter1 = self.MyLogic.GetTimeCounter()
-
-        def calc_single_count(pos, neg):
-            return self.MyLogic.CalcCount(binary_code(pos), binary_code(neg))
 
         if isinstance(channel_pos[0], Sequence):
             if channel_neg == 0:
                 channel_neg = [0]*len(channel_pos)
             counts = []
             for pos, neg in zip(channel_pos, channel_neg):
-                iter_counts = calc_single_count(pos, neg)
+                iter_counts = self.calc_single_count(pos, neg)
                 counts.append(iter_counts)
         else:
             counts = calc_single_count(channel_pos, channel_neg)
         return counts, TimeCounter1
+
+    def read_counts(self, pos_coincidence, pos_singles, neg_singles=0):
+        self.MyLogic.ReadLogic()
+        timecounter = self.MyLogic.GetTimeCounter()
+
+        counts_singles = [None]*len(pos_singles)
+        for k, pos in enumerate(pos_singles):
+            counts_singles[k] = self.calc_single_count(pos, 0)
+
+        counts_coinc = [None]*len(pos_coincidence)
+        for k, pos in enumerate(pos_coincidence):
+            counts_coinc[k] = self.calc_single_count(pos, neg_singles)
+
+        return np.array(counts_coinc, dtype=int), np.array(counts_singles,dtype=int), timecounter
+
+    def antilatch_check(self, singles_to_check):
+        check = [singles==0 for singles in singles_to_check]
+        any_flag = any(check)
+        all_flag = all(check)
+        return any_flag + all_flag
+
+    def read_counts_integrated(self, pos_coincidence, pos_singles, neg_singles=0):
+        iter = 0
+        counting_time = 0
+        total_c_counts = np.zeros(len(pos_coincidence))
+        total_s_counts = np.zeros(len(pos_singles))
+        prev_latched = False
+
+        self.clear_buffer()
+        while counting_time <= self._integration_window:
+            time.sleep(self._antilatch_timeslice)
+            c_counts, s_counts, timecounter = self.read_counts(pos_coincidence=pos_coincidence,
+                                                               pos_singles=pos_singles,
+                                                               neg_singles=neg_singles)
+            antilatch_flags = self.antilatch_check(s_counts)
+            if antilatch_flags == 1:
+                self.antilatch_func()
+                # print('.',end='')
+                time.sleep(0.2)
+                self.clear_buffer()
+                continue
+
+            elif antilatch_flags == 2:
+                print('WARNING: all channels latched, waiting 5 min.')
+                time.sleep(300)
+                self.antilatch_func()
+                # print(',', end='')
+                time.sleep(0.1)
+                self.clear_buffer()
+                continue
+
+            total_c_counts += c_counts
+            total_s_counts += s_counts
+            counting_time += timecounter*5e-9
+        return total_c_counts, total_s_counts, counting_time
 
     def getAvgPhotonCount(self, timeInterval, channel):
         """
